67596e48011a6b61b2c65f3122ba9c5932e51a80
khinnadiko <khin.nadi1@mail.kmutt.ac.th>
2025-03-19 14:11:36 +0700

    Configured routes

--- ai-hub-frontend/src/main.jsx
+++ ai-hub-frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; //import these modules
 import './index.css'
 import App from './App.jsx'
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LandingPage from './pages/LandingPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/", // Home route
+    element: <LandingPage />, // Render the App component
+  },
+  
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
