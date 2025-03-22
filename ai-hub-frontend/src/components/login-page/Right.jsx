2ef9f68e0a9c4d6699471cfee3c5742aa079ea0e
Slade <saib6064@gmail.com>
2025-03-22 18:03:02 +0700

    feat(pages): implement LoginPage

---
+++ ai-hub-frontend/src/components/login-page/Right.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import FormInput from "../signup-page/Forminput";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import Button from "../signup-page/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const Left = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  //navigage
+  const navigate = useNavigate();
+
+  //zod
+  const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+  });
+
+  //useForm
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  //onSubmit
+  const onSubmit = (data) => {
+    console.log("Form data:", data);
+    navigate("/courses");
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center md:justify-center ">
+        <h1 className="font-bold text-2xl">Sign In</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormInput
+            title={"Email"}
+            type={"text"}
+            placeholder={"Enter your email"}
+            {...register("email")}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm mb-1">{errors.email.message}</p>
+          )}
+          <FormInput
+            title={"Password"}
+            type={showPassword ? "text" : "password"}
+            placeholder={"Enter your password"}
+            toggleIcon={showPassword ? faEye : faEyeSlash}
+            onToggle={() => {
+              setShowPassword(!showPassword);
+            }}
+            {...register("password")}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm mb-1">
+              {errors.password.message}
+            </p>
+          )}
+
+          <Button btn={"Login"} />
+          <div className="mt-3 flex flex-row items-center justify-center gap-2 mb-3">
+            <p>Haven't signed up?</p>
+            <p>
+              <Link
+                to="/signup"
+                className="font-bold text-[var(--primary-color)] underline-offset-8 underline"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Left;
