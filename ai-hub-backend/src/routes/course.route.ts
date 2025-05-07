1b6836bbb9efeadad18cf26ed9480e379a6c0b7f
Albert <albert.zaws@mail.kmutt.ac.th>
2025-05-08 00:03:15 +0700

    adding get course by id route

--- ai-hub-backend/src/routes/course.route.ts
+++ ai-hub-backend/src/routes/course.route.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { createCourse } from "../controllers/course/create_course/create.course.ts";
-import { getCourses } from "../controllers/course/get_course.ts/get.course.ts";
-import { deleteCourse } from "../controllers/course/delete_course.ts/delete.course.ts";
+import { getCourses, getCourseById } from "../controllers/course/get_course/get.course.ts";
+import { deleteCourse } from "../controllers/course/delete_course/delete.course.ts";
 
 const courseRouter = new Hono();
 
