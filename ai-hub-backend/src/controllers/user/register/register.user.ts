0de0f6639d3c0dce9a99d8e0d8beff5c1dbd05a3
Slade <saib6064@gmail.com>
2025-05-09 23:36:09 +0700

    feat: implement user logout functionality by deleting the jwt cookie fix(register.user.ts): correct grammar in email verification text

--- ai-hub-backend/src/controllers/user/register/register.user.ts
+++ ai-hub-backend/src/controllers/user/register/register.user.ts
@@ -43,7 +43,7 @@ const registerUser = async (c: Context) => {
       subject: "Email Verification",
 
       // This would be the text of email body
-      text: `Hi! There, You have recently visited 
+      text: `Hi There! You have recently visited 
              our website and entered your email.
              Please follow the given link to verify your email
              http://localhost:${process.env.PORT!}/user/verify/${jwtToken} 
